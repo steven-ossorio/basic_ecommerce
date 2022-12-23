9534478ebf37c62b4051ebd9431e94ad7f53ebf9
Steven <codebusiness2014@gmail.com>
2022-12-22 19:43:47 -0600

    Added fire utils for signin component

--- ecommerce/src/routes/sign-in/sign-in.component.jsx
+++ ecommerce/src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,19 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-  return <div>Sign In</div>;
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <button onClick={() => logGoogleUser()}>Sign In</button>
+    </div>
+  );
 };
 
 export default SignIn;
