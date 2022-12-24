7048e2d7a86eb9872a4523f7cfceb8d0e354b907
Steven <codebusiness2014@gmail.com>
2022-12-24 10:22:25 -0600

    Created authentication component and allowing to signin with email/password with Google

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Authentication from "./routes/authentication/authentication";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-in.component";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="signin" element={<SignIn />} />
+        <Route path="signin" element={<Authentication />} />
       </Route>
     </Routes>
   );
---
+++ ecommerce/src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import {
+  createUserDocumentFromAuth,
+  signinAuthUserWithEmailAndPassword,
+  signInWithGooglePopup,
+} from "../../utils/firebase/firebase.utils";
+import Button from "../button/button.component";
+import FormInput from "../form-input/form-input.component";
+
+import "./sign-in-form.styles.scss";
+
+const defaultFormFields = {
+  email: "",
+  password: "",
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const signInWithGoogle = async () => {
+    console.log("presing sign in with google");
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("pressing");
+
+    try {
+      const res = await signinAuthUserWithEmailAndPassword(email, password);
+      resetFormFields();
+    } catch (error) {
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("Incorrect password for email");
+          break;
+        case "auth/user-not-found":
+          alert("no user associatd with this email");
+          break;
+        default:
+          console.log(error);
+      }
+    }
+  };
+
+  return (
+    <div className="sign-in-container">
+      <h2>Already have an account</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          required
+          label="Email"
+          type="email"
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+        <FormInput
+          required
+          label="Password"
+          type="password"
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+        <div className="buttons-container">
+          <Button type="submit">Sign Up</Button>
+          <Button buttonType="google" type="button" onClick={signInWithGoogle}>
+            Google sign in
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
