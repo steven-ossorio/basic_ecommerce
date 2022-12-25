5167f6a4d3672f3edfee5694f63055b959135801
Steven <codebusiness2014@gmail.com>
2022-12-25 07:55:56 -0600

    Able to view items in cart. Able to add products to cart

--- ecommerce/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ ecommerce/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,18 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button>Check Out</Button>
+        {cartItems.map((item) => (
+          <CartItem cartItem={item} />
+        ))}
       </div>
+      <Button>Go to checkout</Button>
     </div>
   );
 };
