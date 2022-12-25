dff42b11e49715fab6741eba345061ed1761acd9
Steven <codebusiness2014@gmail.com>
2022-12-25 07:15:52 -0600

    Added a basic cart and dropdown

--- ecommerce/src/routes/navigation/navigation.component.jsx
+++ ecommerce/src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,17 @@
 import { Fragment, useContext } from "react";
+
 import { Link, Outlet } from "react-router-dom";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import { CartContext } from "../../contexts/cart.context";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
+
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -23,7 +30,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
