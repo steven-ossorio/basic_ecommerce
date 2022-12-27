3549337114dd0d5033ce610f5375e76a8dea099d
Steven <codebusiness2014@gmail.com>
2022-12-27 07:58:58 -0600

    Finish shop checkout. Changing SCSS to Styled-component for a specific reason as clashes avoidence

---
+++ ecommerce/src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,20 @@
+import "./directory-item.styles.scss";
+
+const DirectoryItem = ({ category: { id, title, imageUrl } }) => {
+  return (
+    <div key={id} className="directory-item-container">
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="body">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default DirectoryItem;
--- ecommerce/src/routes/navigation/navigation.component.jsx
+++ ecommerce/src/routes/navigation/navigation.component.jsx
@@ -7,7 +7,12 @@ import CartIcon from "../../components/cart-icon/cart-icon.component";
 import { CartContext } from "../../contexts/cart.context";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
-import "./navigation.styles.scss";
+import {
+  NavigationContainer,
+  NavLinks,
+  NavLink,
+  LogoContainer,
+} from "./navigation.styles";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -15,25 +20,21 @@ const Navigation = () => {
 
   return (
     <Fragment>
-      <div className="navigation">
-        <div className="logo-container">Logo</div>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/shop">
-            SHOP
-          </Link>
-          {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>
-              Sign out
-            </span>
-          ) : (
-            <Link className="nav-link" to="/signin">
-              SIGN IN
-            </Link>
-          )}
-          <CartIcon />
-        </div>
+      <LogoContainer to="/">Logo</LogoContainer>
+      <NavigationContainer>
+        <NavLinks>
+          <NavLink to="/shop">SHOP</NavLink>
+        </NavLinks>
+        {currentUser ? (
+          <NavLink as="span" onClick={signOutUser}>
+            Sign Out
+          </NavLink>
+        ) : (
+          <NavLink to="/signin">Sign In</NavLink>
+        )}
+        <CartIcon />
         {isCartOpen && <CartDropdown />}
-      </div>
+      </NavigationContainer>
       <Outlet />
     </Fragment>
   );
