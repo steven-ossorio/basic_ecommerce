ef56b81e42290257aa0941b1b809738034a73ef9
Steven <codebusiness2014@gmail.com>
2022-12-28 00:02:48 -0600

    Updating from scss to styled-components

--- ecommerce/src/components/directory-item/directory-item.component.jsx
+++ ecommerce/src/components/directory-item/directory-item.component.jsx
@@ -1,19 +1,22 @@
-import "./directory-item.styles.scss";
+import {
+  BackgroundImage,
+  Body,
+  DirectoryItemContainer,
+} from "./directory-item.styles";
 
 const DirectoryItem = ({ category: { id, title, imageUrl } }) => {
   return (
-    <div key={id} className="directory-item-container">
-      <div
-        className="background-image"
+    <DirectoryItemContainer key={id}>
+      <BackgroundImage
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className="body">
+      <Body to={`/shop/${title}`}>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
+      </Body>
+    </DirectoryItemContainer>
   );
 };
 
