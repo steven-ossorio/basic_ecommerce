6fe72763134d8667ea4b6e9cf81575211b9daad3
Steven <codebusiness2014@gmail.com>
2022-12-29 06:48:52 -0600

    Directory titles uppercased

--- ecommerce/src/components/directory-item/directory-item.component.jsx
+++ ecommerce/src/components/directory-item/directory-item.component.jsx
@@ -9,7 +9,7 @@ const DirectoryItem = ({ category: { id, title, imageUrl } }) => {
     <DirectoryItemContainer key={id}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body to={`/shop/${title}`}>
-        <h2>{title}</h2>
+        <h2>{title.toUpperCase()}</h2>
         <p>Shop Now</p>
       </Body>
     </DirectoryItemContainer>
