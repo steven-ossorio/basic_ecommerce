bc8df416319dd8287b04639e5c32a6421b736228
Steven <codebusiness2014@gmail.com>
2023-01-02 07:20:07 -0600

    Working on adding stripe test to ecommerce checkout

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -3,19 +3,22 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
+import { Elements } from "@stripe/react-stripe-js";
 import App from "./App";
 
 import "./index.css";
 import { store, persistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
-
+import { stripePromise } from "./utils/stripe.utils";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
